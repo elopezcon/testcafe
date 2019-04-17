adeea6509176ca69293335cd71af44ed95e3ee35
Gustav <glopezmq@gmail.com>
2019-04-17 16:50:10 -0500

    Change selectors and remove unnecessary code

--- functionalTest/objectPages/myNotesPage.js
+++ functionalTest/objectPages/myNotesPage.js
@@ -1,7 +1,7 @@
 import { Selector } from 'testcafe'
 
 export default class MyNotesPage {
-    constructor(){
-        this.myNotes = Selector('#my-notes-page h2');
+    constructor() {
+        this.myNotes = Selector('#my-notes-page').find('h2');
     }
 }
